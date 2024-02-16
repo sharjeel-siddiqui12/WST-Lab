f1bc3d0f9a08c0d7960bfa8deef3c69da14bc444
sharjeel-siddiqui12 <sharjeelsiddiqui00@gmail.com>
2024-02-16 18:57:28 +0500

    updated

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+fetch('/assets/header.html')
+.then(response => response.text())
+.then(html => {
+    document.body.insertAdjacentHTML('afterbegin', html);
+})
+.catch(error => console.error('Error fetching header:', error));
+
+
+// favicon.js
+
+(function() {
+    var link = document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'icon';
+    link.href = '/assets/img/logo.ico';
+    document.getElementsByTagName('head')[0].appendChild(link);
+})();
